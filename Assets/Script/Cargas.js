9062e9afadefb34aaa28ffac19d225caad943248
Oscar Estaban Ramirez Cely <xirious@hotmail.com>
2015-08-31 22:29:55 -0500

    Primer Commit: Oscar Ramirez

---
+++ Assets/Script/Cargas.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update(){
+
+}
+
+function Princi() {
+Application.LoadLevel("PR");
+}
+
+function P1() {
+Application.LoadLevel("PA");
+}
+function P2() {
+Application.LoadLevel("PB");
+}
+function P3() {
+Application.LoadLevel("PC");
+}
